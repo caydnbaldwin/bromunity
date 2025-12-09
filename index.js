56b4c2a8a2d58bc5ffa64c5fa6b9d13b3a4488ec
peteryoungbyu <peteryoung.byu@gmail.com>
2025-12-09 16:48:19 -0700

    I DID SOME KOOL STUFF

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ let app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 app.use(
   session(
     {
